9b3b8f23bbeb445c7c0d2af31e3f5f207c2c4bfa
Logan Barsell <loganjbars@gmail.com>
2021-10-30 19:10:24 -0700

    refactor

---
+++ src/bootstrap.js
@@ -0,0 +1,36 @@
+import faker from 'faker';
+
+
+const mount = (el) => {
+    let products = '';
+
+    for (let i = 0; i < 5; i++) {
+        const name = faker.commerce.productName();
+        products += `<div>${name}</div>`;
+    }
+
+    el.innerHTML = products;
+};
+
+// Context.Situation #1
+// We are running this file in development in isolation
+// We are using our local index.html file
+// Which DEFINITELY has an element with id 'dev-products'
+// We want to immediately render our app into that element
+if(process.env.NODE_ENV === 'development') {
+    const el = document.querySelector('#dev-products');
+    
+    // Assuming our container doesnt have an element with id 'dev-products'..
+    if (el) {
+        //We are probably running in isolation
+        mount(el);
+    }
+}
+
+// Context/Situation #2
+// We are running this file in development or production
+// through the CONTAINER app
+// NO GUARENTEE that an element with id of 'dev-products' exists
+// WE DO NOT WANT to immediately render the app
+
+export { mount };
